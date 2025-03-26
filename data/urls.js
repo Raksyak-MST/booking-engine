c003624ae8339884d414a24ba25a499571f01212
rakshyak <rakshak1998@gmail.com>
2025-03-26 17:11:34 +0530

    feat(utility): add api urls, and date formatter

---
+++ data/urls.js
@@ -0,0 +1,4 @@
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL
+export const URL  = {
+    LOGIN: BASE_URL.concat("/webLogin")
+}
--- utils/textFormatter.js
+++ utils/textFormatter.js
@@ -1,3 +1,5 @@
+import moment from "moment"
+
 export const pluralRule = function (value) {
   const formatter = new Intl.PluralRules("en", { type: "cardinal" });
   const mapper = {
@@ -6,3 +8,9 @@ export const pluralRule = function (value) {
   };
   return mapper[formatter.select(value)]
 };
+
+
+export const dateFormatter = function(date, defaultFormat="YYYY-MM-DD") {
+  const formatter = moment(date);
+  return formatter.format(defaultFormat)
+}
