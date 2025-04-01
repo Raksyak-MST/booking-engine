f62ff2fca98b9d148abb0bfdda75f413e3041938
rakshyak <rakshak1998@gmail.com>
2025-04-01 13:33:20 +0530

    fix(ui): changed the top level room type detail alignment

--- components/booking-page/sidebar/BookingDetails.jsx
+++ components/booking-page/sidebar/BookingDetails.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { useSelector } from "react-redux"
+import moment from "moment"
 
 const BookingDetails = () => {
   const selectedRoom = useSelector(state => state.roomSelection)
@@ -7,8 +8,8 @@ const BookingDetails = () => {
   return (
     <div className="px-30 py-30 border-light rounded-4">
       <div className="text-20 fw-500 mb-30">Your booking details</div>
-      <div className="x-gap-15 y-gap-20">
-        <div>
+      <div className="row x-gap-15 y-gap-20">
+        <div className="col-auto">
           <Image
             width={140}
             height={140}
@@ -19,9 +20,27 @@ const BookingDetails = () => {
         </div>
         {/* End .col */}
         <div className="col">
+          <div className="d-flex x-gap-5 pb-10">
+            <i className="icon-star text-yellow-1 text-10" />
+            <i className="icon-star text-yellow-1 text-10" />
+            <i className="icon-star text-yellow-1 text-10" />
+            <i className="icon-star text-yellow-1 text-10" />
+            <i className="icon-star text-yellow-1 text-10" />
+          </div>
+          {/* End ratings */}
           <div className="lh-17 fw-500">{selectedRoom?.roomTypeName}</div>
-          <div className="text-14 text-light-1 lh-15 mt-5">
-            {selectedRoom?.description}
+          <div className="row x-gap-10 y-gap-10 items-center pt-10">
+            <div className="col-auto">
+              <div className="d-flex items-center">
+                <div className="size-30 flex-center bg-blue-1 rounded-4">
+                  <div className="text-12 fw-600 text-white">4.8</div>
+                </div>
+                <div className="text-14 fw-500 ml-10">Exceptional</div>
+              </div>
+            </div>
+            <div className="col-auto">
+              <div className="text-14">3,014 reviews</div>
+            </div>
           </div>
         </div>
         {/* End .col */}
@@ -32,14 +51,14 @@ const BookingDetails = () => {
       <div className="row y-gap-20 justify-between">
         <div className="col-auto">
           <div className="text-15">Check-in</div>
-          <div className="fw-500">{selectedRoom?.arrivalDate}</div>
+          <div className="fw-500">{moment(selectedRoom?.arrivalDate).format('ddd DD MMM YYYY')}</div>
         </div>
         <div className="col-auto md:d-none">
           <div className="h-full w-1 bg-border" />
         </div>
         <div className="col-auto text-right md:text-left">
           <div className="text-15">Check-out</div>
-          <div className="fw-500">{selectedRoom?.departureDate}</div>
+          <div className="fw-500">{moment(selectedRoom?.departureDate).format('ddd DD MMM YYYY')}</div>
         </div>
       </div>
       {/* End row */}
