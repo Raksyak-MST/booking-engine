16b63a110d687bc208bc05b5df3fcd1d0c661b3e
rakshyak <rakshak1998@gmail.com>
2025-04-08 12:20:20 +0530

    remove login icon from mobile view

--- components/header/header-11/index.jsx
+++ components/header/header-11/index.jsx
@@ -47,12 +47,6 @@ const Header1 = () => {
               {/* Start mobile menu icon */}
               <div className="d-none xl:d-flex x-gap-20 items-center pl-30 text-white">
                 <div>
-                  <Link
-                    href="/login"
-                    className="d-flex items-center icon-user text-inherit text-22"
-                  />
-                </div>
-                <div>
                   <button
                     className="d-flex items-center icon-menu text-inherit text-20"
                     data-bs-toggle="offcanvas"
