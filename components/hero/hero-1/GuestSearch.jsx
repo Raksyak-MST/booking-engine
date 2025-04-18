98293a30453c12f3bc28dd56e048946330baeb60
Raksyak-MST <rakshyak@ms-tech.in>
2025-04-18 16:21:31 +0530

    refactor the Guest selection

--- components/hero/hero-1/GuestSearch.jsx
+++ components/hero/hero-1/GuestSearch.jsx
@@ -5,59 +5,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { bookingQueryActions, roomPickActions } from "@/store/store";
 import { useEffect } from "react";
 
-const Counter = ({
-  name,
-  title,
-  defaultValue,
-  count,
-  increment = () => {},
-  decrement = () => {},
-  other = {},
-}) => {
-  return (
-    <>
-      <div className="row y-gap-10 justify-between items-center">
-        <div className="col-auto">
-          <div className="text-15 lh-12 fw-500">{title}</div>
-          {title === "Children" && (
-            <div className="text-14 lh-12 text-light-1 mt-5">{other?.rule}</div>
-          )}
-          <div className="text-12 text-light-1">{other?.description}</div>
-        </div>
-        {/* End .col-auto */}
-        <div className="col-auto">
-          <div className="d-flex items-center js-counter">
-            <button
-              className="button -outline-blue-1 text-blue-1 size-38 rounded-4 js-down"
-              onClick={decrement}
-            >
-              <i className="icon-minus text-12" />
-            </button>
-            {/* decrement button */}
-            <div className="flex-center size-20 ml-15 mr-15">
-              <div className="text-15 js-count">{count}</div>
-            </div>
-            {/* counter text  */}
-            <button
-              className="button -outline-blue-1 text-blue-1 size-38 rounded-4 js-up"
-              onClick={increment}
-            >
-              <i className="icon-plus text-12" />
-            </button>
-            {/* increment button */}
-          </div>
-        </div>
-        {/* End .col-auto */}
-      </div>
-      {/* End .row */}
-      <div className="border-top-light mt-24 mb-24" />
-    </>
-  );
-};
-
 const GuestSearch = () => {
   const state = useSelector((state) => state.bookingQuery);
+  const roomPicked = useSelector((state) => state.roomPick?.roomPicked);
   const dispatch = useDispatch();
+
+  console.log(roomPicked);
 
   useEffect(() => {
     // FIXME: create a new room option after the state is updated this ensures
@@ -69,6 +22,10 @@ const GuestSearch = () => {
       }),
     );
   }, [state?.quantity]);
+
+  const handleAddRoom = () => {
+    dispatch(roomPickActions.addRoom());
+  };
 
   return (
     <div className="searchMenu-guests px-30 lg:py-20 lg:px-0 js-form-dd js-form-counters position-relative">
@@ -89,56 +46,49 @@ const GuestSearch = () => {
 
       <div className="shadow-2 dropdown-menu min-width-400">
         <div className="bg-white px-30 py-30 rounded-4 counter-box">
-          <Counter
-            name="adults"
-            title="Adults"
-            defaultValue={1}
-            count={state?.adults}
-            increment={(e) =>
-              dispatch(bookingQueryActions.onAdultChange(state?.adults + 1))
-            }
-            decrement={(e) =>
-              dispatch(bookingQueryActions.onAdultChange(state?.adults - 1))
-            }
-            other={{ description: "Per room max 2 Adults" }}
-          />
-          <Counter
-            name="children"
-            title="Children"
-            defaultValue={state?.children}
-            count={state?.children}
-            increment={(e) => {
-              dispatch(
-                bookingQueryActions.onChildrenChange(state?.children + 1),
-              );
-            }}
-            decrement={(e) =>
-              dispatch(
-                bookingQueryActions.onChildrenChange(state?.children - 1),
-              )
-            }
-            other={{
-              description: "Per room max 3 Children",
-              rule: "Ages 0 - 17",
-            }}
-          />
-          <Counter
-            name="quantity"
-            title="Rooms"
-            defaultValue={1}
-            count={state?.quantity}
-            increment={(e) => {
-              dispatch(bookingQueryActions.onRoomChange(state?.quantity + 1));
-            }}
-            decrement={(e) => {
-              dispatch(roomPickActions.removeRoom());
-              dispatch(
-                bookingQueryActions.onRoomChange(
-                  Math.max(1, state?.quantity - 1),
-                ),
-              );
-            }}
-          />
+          <h2 className="text-14 textkok-light-1 text-center">
+            Rooms and guests
+          </h2>
+          <div className="border-top-light mt-28 mb-20" />
+          <div className="d-flex justify-content-between mb-20">
+            <div>Rooms</div>
+            <div>Adults</div>
+            <div>Children</div>
+          </div>
+          {roomPicked.map((id, index) => (
+            <div className="text-12">
+              <div className="d-flex gap-2 align-items-center mb-2 justify-content-between">
+                <div className="d-flex gap-2">
+                  <button className="btn border text-12">X</button>
+                  <span>Room {index + 1}</span>
+                </div>
+                <div className="d-flex gap-2 ">
+                  <button className="btn border text-12">
+                    <i className="icon-plus" />
+                  </button>
+                  <span>{state?.adults}</span>
+                  <buttoni className="btn border text-12">
+                    <i className="icon-minus" />
+                  </buttoni>
+                </div>
+                <div className="d-flex gap-2">
+                  <button className="btn border text-12">
+                    <i className="icon-plus" />
+                  </button>
+                  <span>{state?.children}</span>
+                  <buttoni className="btn border text-12">
+                    <i className="icon-minus" />
+                  </buttoni>
+                </div>
+              </div>
+            </div>
+          ))}
+          <div className="border-top-light mt-28" />
+          <button className="text-14" onClick={handleAddRoom}>
+            <i className="icon-plus m-2" />
+            <span>Add rooms</span>
+          </button>
+          <div className="border-top-light" />
         </div>
       </div>
     </div>
