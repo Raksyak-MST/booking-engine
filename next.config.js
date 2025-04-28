72cfe274015b64110edfaf8dad07d2fe8fe59637
Raksyak-MST <rakshyak@ms-tech.in>
2025-04-28 18:46:41 +0530

    Fix formatting in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        // domains: ["theoterra.com"],
-        remotePatterns: [
-            {
-                hostname: "theoterra.com",
-                pathname: "**"
-            }
-        ]
-    }
-}
+  images: {
+    // domains: ["theoterra.com"],
+    remotePatterns: [
+      {
+        hostname: "theoterra.com",
+        pathname: "**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
